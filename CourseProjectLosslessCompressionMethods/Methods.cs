8c58ace70642a203cf07d116fc998d2bd8c33fa5
ch1llay <ch1llay@yandex.ru>
2021-11-20 17:14:50 +0300

    Добавьте файлы проекта.

---
+++ CourseProjectLosslessCompressionMethods/Methods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProjectLosslessCompressionMethods
+{
+    class Methods
+    {
+        static Dictionary<byte, float> HuffmansCodes = new Dictionary<byte, float>();
+        static void Main()
+        {
+            byte[] image = File.ReadAllBytes("image.jpg");
+            PrepareDictionary();
+            GetFreqSymbol(image);
+
+        }
+
+        static void PrepareDictionary()
+        {
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                HuffmansCodes.Add(i, 0);
+            }
+        }
+        static void GetFreqSymbol(byte[] bytes)
+        {
+            bytes = bytes.OrderBy(x => x).ToArray();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (HuffmansCodes.ContainsKey(bytes[i]))
+                {
+                    HuffmansCodes[bytes[i]]++;
+                }
+            }
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (HuffmansCodes.ContainsKey(bytes[i]))
+                {
+                    HuffmansCodes[bytes[i]] /= bytes.Length;
+                }
+            }
+            foreach (var pair in HuffmansCodes.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{pair.Key} {pair.Value}");
+            }
+        }
+    }
+}
