d5561b8214c2e2c1e0f675e95a567f4ad35ff88e
ch1llay <ch1llay@yandex.ru>
2021-12-29 01:59:44 +0300

    commit

--- CourseProjectLosslessCompressionMethods/OptimazeLz77.cs
+++ CourseProjectLosslessCompressionMethods/OptimazeLz77.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace CourseProjectLosslessCompressionMethods
             Head += count;
             Tail += count;
         }
-        public int Differense { get => Head - Tail+1; }
+        public int Differense { get => Head - Tail + 1; }
     }
     class Offset
     {
@@ -38,22 +39,20 @@ namespace CourseProjectLosslessCompressionMethods
 
     class OptimazeLz77
     {
-        string inputData;
+        byte[] inputData;
         int sizeInputData;
         static int buferSize;
         static int dictSize;
         Part bufer;
         Part dict;
-        List<string> code;
-        public OptimazeLz77(int buferSize_, int dictSize_, string inputData_)
+        List<byte[]> code;
+        public OptimazeLz77(int buferSize_, int dictSize_)
         {
             buferSize = buferSize_;
             dictSize = dictSize_;
-            this.inputData = inputData_;
-            code = new List<string>();
+            code = new List<byte[]>();
             bufer = new Part(buferSize, -1, -buferSize);
             dict = new Part(dictSize, -buferSize - 1, -buferSize - 1 - dictSize);
-            sizeInputData = inputData.Length;
 
         }
         void CommonSlide(int count)
@@ -67,7 +66,7 @@ namespace CourseProjectLosslessCompressionMethods
             byte pos = 0;
             if (dict.Head >= 0)
             {
-                int dictIndex = (dict.Tail >= 0) ? -1 : -dict.Tail-1;
+                int dictIndex = (dict.Tail >= 0) ? -1 : -dict.Tail - 1;
                 int tempDictIndex = (dict.Tail >= 0) ? dict.Tail : 0;
                 for (int idict = tempDictIndex; idict <= dict.Head; idict++, dictIndex++)
                 {
@@ -76,7 +75,7 @@ namespace CourseProjectLosslessCompressionMethods
                         tempDictIndex = idict;
                         int buferIndex = bufer.Tail;
                         pos = (byte)dictIndex;
-                        while (idict < dict.Head && buferIndex < bufer.Head && buferIndex < sizeInputData-1 && inputData[tempDictIndex] == inputData[buferIndex])
+                        while (idict < dict.Head && buferIndex < bufer.Head && buferIndex < sizeInputData - 1 && inputData[tempDictIndex] == inputData[buferIndex])
                         {
                             count++;
                             tempDictIndex++;
@@ -86,21 +85,75 @@ namespace CourseProjectLosslessCompressionMethods
                     }
                 }
             }
+            if(count == 0)
+            {
+                pos = 0;
+            }
             return new Offset(pos, count);
+        }
+        string to2(byte n)
+        {
+            string s = Convert.ToString(n, 2);
+            return s;
+
+
+        }
+        byte[] Bits2Bytes()
+        {
+            byte sum = 0;
+            byte bit = 1;
+            List<byte> bits = new List<byte>();
+            foreach (byte[] data in code)
+            {
+                foreach (byte b in data)
+                {
+                    foreach (var c in to2(b))
+                    {
+                        if (c == '1')
+                        {
+                            sum |= bit;
+                        }
+                        if (bit < 128)
+                        {
+                            bit <<= 1;
+                        }
+                        else
+                        {
+                            bits.Add(sum);
+                            sum = 0;
+                            bit = 1;
+                        }
+
+                    }
+                }
+            }
+            return bits.ToArray();
+        }
+        public void CompressFile(string inputFilename)
+        {
+            inputData = File.ReadAllBytes(inputFilename);
+            sizeInputData = inputData.Length;
+            Coding();
+            byte[] outputData = Bits2Bytes();
+            File.WriteAllBytes($"{inputFilename}.lz77", outputData);
+
         }
         public void Coding()
         {
             Offset offset;
             CommonSlide(buferSize);
-            while (dict.Head < sizeInputData-1)
+            while (dict.Head < sizeInputData - 1)
             {
                 offset = GetOffset();
                 CommonSlide(offset.count + 1);
-                code.Add($"({offset.pos} {offset.count} {inputData[dict.Head]}");
+                byte[] tempBytes = new byte[] { offset.pos, offset.count, inputData[dict.Head] };
+                Console.WriteLine($"({offset.pos}, {offset.count}, {(char)inputData[dict.Head]})");
+                code.Add(tempBytes);
 
             }
         }
 
     }
-
 }
+
+
